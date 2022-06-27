1401eb94204247e29557d37a5498b6236ecb28a2
musleh0001 <khan.muslehuddin0001@gmail.com>
2022-06-27 17:40:28 +0600

    fix: leaflet map

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -22,15 +22,11 @@ const About = () => {
 						<AnimatedLetters letterClass={letterClass} strArray={["A", "b", "o", "u", "t", " ", "m", "e"]} idx={15} />
 					</h1>
 					<p>
-						I'm very ambitious front-end developer looking for a role in established IT company with the opportunity to work with the
+						I'm very ambitious full stack developer looking for a role in established IT company with the opportunity to work with the
 						latest technologies on challenging and diverse projects.
 					</p>
 					<p align="LEFT">
 						I'm quietly confident, naturally curious, and perpetually working on improving my chops one design problem at a time.
-					</p>
-					<p>
-						If I need to define myself in one sentence that would be a family person, father of a beautiful daughter, a sports fanatic,
-						photography enthusiast, and tech-obsessed!!!
 					</p>
 				</div>
 
